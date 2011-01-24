8a7771d6a8b896d30b36a0b6d5530cf55fd2d007
Ryan Krauss <ryanlists@gmail.com>
2011-01-24 11:08:11 -0600

    added spring_2011_484.py

---
+++ spring_2011_484.py
@@ -0,0 +1,51 @@
+import os
+import rwkos, spreadsheet
+
+monday_groups = ['Lead Shot Dripping System', \
+                 'Solar Car Chassis', \
+                 'Remote Controlled Retrieval Vehicle', \
+                 'Telescope Shelter', \
+                 'Baja Transaxle']
+
+wednesday_groups = ['Mechanized Bicycle Rack and Storage', \
+                    'Adjustable Ratio Rocker Arms', \
+                    'Automatic Tennis Ball Shooter', \
+                    'Foam Dart Claymore', \
+                    'Trash Master', \
+                    'Screw Gun Cooling System']
+
+all_groups = monday_groups + wednesday_groups
+
+class_folder = rwkos.FindFullPath('siue/classes/484/2011')
+group_path = os.path.join(class_folder, 'group_list.csv')
+group_list = spreadsheet.group_list(group_path)
+
+alts = {'Bemrose-Fetter':'Rebecca', \
+        'Brown':'Chris',\
+        'Flahan':'Chris',\
+        'Card':'Will',\
+        'Dement':'Joe',\
+        'Gehrs':'Ben',\
+        'Graham':'Mike',\
+        'Mulvey':'Dan',\
+        'Hankins':'Matt',\
+        'Pish':'Jill',\
+        'Spihlman':'Andi',\
+        'Thompson':'Luke',\
+        'Woodrome':'Jon',\
+        }
+
+inverse_alts = {'Joe':'Joseph', \
+                'Mike':'Michael', \
+                'Luke':'Lucas', \
+                'Andi':'Andrea', \
+                'Matt':'Matthew',\
+                'Jill':'Jillian'}
+
+email_path = os.path.join(class_folder, 'email_list_editted.csv')
+email_list = spreadsheet.email_list(email_path)
+
+
+def make_filename(projectname):
+    filename = projectname.replace(' ','_') + '.rst'
+    return filename
