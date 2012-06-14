cfa2c052b20f91d584b9e688c2609a44620556b2
Ryan Krauss <ryanlists@gmail.com>
2012-06-14 00:14:48 -0500

    tweaked robotics.py to include Rx, Ry, Rz, and HTz, HTx, and HTz

--- robotics.py
+++ robotics.py
@@ -18,6 +18,18 @@ def rot3by3(axis,angle):
     return R
 
 
+def Rz(theta):
+    return rot3by3('z',theta)
+
+
+def Rx(theta):
+    return rot3by3('x',theta)
+
+
+def Ry(theta):
+    return rot3by3('y',theta)
+
+
 def HT4(axis='',angle=0,x=0.,y=0.,z=0.):
     #4x4 homogenous transformation matrix
     if axis:
@@ -28,3 +40,26 @@ def HT4(axis='',angle=0,x=0.,y=0.,z=0.):
     s1=c_[r1,array([[x],[y],[z]])]
     return r_[s1,array([[0.,0.,0.,1.]])]
 
+
+def HTinv(Tin):
+    Tout = eye(4)
+    R = Tin[0:3,0:3]
+    Ri = R.T
+    Tout[0:3,0:3] = Ri
+    P_BorgA = Tin[0:3,3]
+    P_AorgB = -1.0*dot(Ri,P_BorgA)
+    Tout[0:3,3] = P_AorgB
+    return Tout
+
+
+
+def HTz(angle=0,x=0.,y=0.,z=0.):
+    return HT4('z',angle=angle, x=x, y=y, z=z)
+
+
+def HTx(angle=0,x=0.,y=0.,z=0.):
+    return HT4('x',angle=angle, x=x, y=y, z=z)
+
+
+def HTy(angle=0,x=0.,y=0.,z=0.):
+    return HT4('y',angle=angle, x=x, y=y, z=z)
