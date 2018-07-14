94ea30493d6d1faa1bd1b73415d731afc4d0c803
Ryan Krauss <ryanwkrauss@gmail.com>
2018-07-14 11:33:09 -0400

    adding bb_utils.py with blackboard link maker for pdfs

---
+++ bb_utils.py
@@ -0,0 +1,10 @@
+import re
+
+html_pdf_links_str = r"""<p>Open: <a href="https://drive.google.com/open?id=IDSTR" target="_blank">https://drive.google.com/open?id=IDSTR</a></p>
+<p>Download: <a href="https://drive.google.com/uc?export=download&id=IDSTR" target="_blank">https://drive.google.com/uc?export=download&id=IDSTR</a></p>"""
+
+
+def pdf_link_download_maker(linkin):
+    base, linkid = linkin.split("id=",1)
+    out_str = html_pdf_links_str.replace("IDSTR",linkid)
+    print(out_str)
